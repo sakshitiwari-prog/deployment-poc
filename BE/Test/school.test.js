4f0a2f2586a77b1b8964384c9acf176daf1c9f36
sakshitiwari <sakshit3849@gmail.com>
2026-09-08 18:48:30 +0530

    added test group for school, permission

--- BE/Test/school.test.js
+++ BE/Test/school.test.js
@@ -1,123 +1,185 @@
-const { addSchoolList,getPermissionList,addPermissionList } = require("../Controller/school");
-const {School} = require("../Schema/School");
-const {Permission}=require("../Schema/Permission")
+const {
+  addSchoolList,
+  userInfo,
+  getSchoolList,
+  getPermissionList,
+  addPermissionList,
+} = require("../Controller/school");
+const { School } = require("../Schema/School");
+const { Permission } = require("../Schema/Permission");
 jest.mock("../Schema/School");
-jest.mock("../Schema/Permission")
-describe("add school",()=>{
-    test("should add a school",async()=>{
-        School.create.mockResolvedValue({
-            _id:"123",
-            name:"test school",
-            email:"test@gmail.com"
-        })
-        const req={
-            body:{
-                name:"test school",
-                email:"test@gmail.com"
-            }
-        }
-        const res={
-            json:jest.fn()
-        }
-        await addSchoolList(req,res)
+jest.mock("../Schema/Permission");
+describe("add school", () => {
+  test("should add a school", async () => {
+    School.create.mockResolvedValue({
+      _id: "123",
+      name: "test school",
+      email: "test@gmail.com",
+    });
+    const req = {
+      body: {
+        name: "test school",
+        email: "test@gmail.com",
+      },
+    };
+    const res = {
+      json: jest.fn(),
+    };
+    await addSchoolList(req, res);
 
-        expect(School.create).toHaveBeenCalledWith({
-            name:"test school",
-            email:"test@gmail.com"
-        })
-        expect(res.json).toHaveBeenCalledWith({
-            data:{
-                _id:"123",
-                name:"test school",
-                email:"test@gmail.com"
-            }
-        })
-    })
-    test("should handle error",async()=>{
-        School.create.mockRejectedValue(new Error("Database error"))
-        const req={
-            body:{
-                name:"test school",
-                email:"test@gmail.com"
-            }
-        }
-        const res={
-            json:jest.fn()
-        }
-        await expect(addSchoolList(req,res)).rejects.toThrow('Database error')
-    })
-})
+    expect(School.create).toHaveBeenCalledWith({
+      name: "test school",
+      email: "test@gmail.com",
+    });
+    expect(res.json).toHaveBeenCalledWith({
+      data: {
+        _id: "123",
+        name: "test school",
+        email: "test@gmail.com",
+      },
+    });
+  });
+  test("should handle error", async () => {
+    School.create.mockRejectedValue(new Error("Database error"));
+    const req = {
+      body: {
+        name: "test school",
+        email: "test@gmail.com",
+      },
+    };
+    const res = {
+      json: jest.fn(),
+    };
+    await expect(addSchoolList(req, res)).rejects.toThrow("Database error");
+  });
+});
 
 // test group for getPermissionList
-describe("get permission list",()=>{
-    test("should give permission list",async()=>{
-        const res={
-            json:jest.fn()
-        }
-        const commonRes=[
-            {
-                _id:"1",
-                name:"users:read",
-                __v:0,
-            },
-            {
-                _id:"2",
-                name:"users:write",
-                __v:0,
-            },
-        ]
-        Permission.find.mockResolvedValue(commonRes)
-        const req={}
-        await getPermissionList(req,res)
-        expect(res.json).toHaveBeenCalledWith({
-            data:commonRes
-        })
-    })
-    test("should handle error",async()=>{
-        const req={}
-        Permission.find.mockRejectedValue(new Error("DB error"))
-        const res={
-            json:jest.fn()
-        }
-        await expect(getPermissionList(req,res)).rejects.toThrow('DB error')
-    })
-})
+describe("get permission list", () => {
+  test("should give permission list", async () => {
+    const res = {
+      json: jest.fn(),
+    };
+    const commonRes = [
+      {
+        _id: "1",
+        name: "users:read",
+        __v: 0,
+      },
+      {
+        _id: "2",
+        name: "users:write",
+        __v: 0,
+      },
+    ];
+    Permission.find.mockResolvedValue(commonRes);
+    const req = {};
+    await getPermissionList(req, res);
+    expect(res.json).toHaveBeenCalledWith({
+      data: commonRes,
+    });
+  });
+  test("should handle error", async () => {
+    const req = {};
+    Permission.find.mockRejectedValue(new Error("DB error"));
+    const res = {
+      json: jest.fn(),
+    };
+    await expect(getPermissionList(req, res)).rejects.toThrow("DB error");
+  });
+});
 // test group for addPermissionList
-describe("add permission list",()=>{
-    test("should add permission list",async()=>{
-    
-        const commonRes={
-            _id:"1",
-            name:"users:read",
-            __v:0,  
-        }
-        Permission.create.mockResolvedValue(commonRes)
-       const req={
-            body:{
-                name:"users:read",
-            }
-        }
-        const res={
-            json:jest.fn()
-        }
-        await addPermissionList(req,res)
-        expect(Permission.create).toHaveBeenCalledWith({
-                name:"users:read"
-            })
-        expect(res.json).toHaveBeenCalledWith({
-            data:commonRes
-        })
-    })
-    test("should handle error",async()=>{
-        const req={
-            body:{
-                name:"users:read",
-            }
-        }
-        Permission.create.mockRejectedValue(new Error("DB error"))
-        const res={
-            json:jest.fn()
-        }
-        await expect(addPermissionList(req,res)).rejects.toThrow('DB error')
-    })
-})
+describe("add permission list", () => {
+  test("should add permission list", async () => {
+    const commonRes = {
+      _id: "1",
+      name: "users:read",
+      __v: 0,
+    };
+    Permission.create.mockResolvedValue(commonRes);
+    const req = {
+      body: {
+        name: "users:read",
+      },
+    };
+    const res = {
+      json: jest.fn(),
+    };
+    await addPermissionList(req, res);
+    expect(Permission.create).toHaveBeenCalledWith({
+      name: "users:read",
+    });
+    expect(res.json).toHaveBeenCalledWith({
+      data: commonRes,
+    });
+  });
+  test("should handle error", async () => {
+    const req = {
+      body: {
+        name: "users:read",
+      },
+    };
+    Permission.create.mockRejectedValue(new Error("DB error"));
+    const res = {
+      json: jest.fn(),
+    };
+    await expect(addPermissionList(req, res)).rejects.toThrow("DB error");
+  });
+});
+
+// test user info
+describe("user info", () => {
+  test("should give user info", async () => {
+    const req = {};
+    const res = {
+      json: jest.fn(),
+    };
+    await userInfo(req, res);
+    expect(res.json).toHaveBeenCalledWith({
+      data: "user info",
+    });
+  });
+  test("should handle error", async () => {
+    const req = {};
+    const res = {
+      json: jest.fn().mockImplementation(() => {
+        throw new Error("DB error");
+      }),
+    };
+    await expect(userInfo(req, res)).rejects.toThrow("DB error");
+  });
+});
+
+describe("getschool list", () => {
+  test("should give school list", async () => {
+    const req = {};
+    School.find.mockResolvedValue({
+      _id: "1",
+      name: "test school",
+      email: "test@gmail.com",
+      __v: 0,
+    });
+    const res = {
+      json: jest.fn(),
+    };
+    await getSchoolList(req, res);
+    expect(School.find).toHaveBeenCalledWith(req);
+    expect(res.json).toHaveBeenCalledWith({
+      data: {
+        _id: "1",
+        name: "test school",
+        email: "test@gmail.com",
+        __v: 0,
+      },
+    });
+  });
+
+  test("should handle error", async () => {
+    const req = {};
+    School.find.mockRejectedValue(new Error("DB error"));
+    const res = {
+      json: jest.fn(),
+    };
+    await expect(getSchoolList(req, res)).rejects.toThrow("DB error");
+  });
+});
